9e829b7eb5896727e0099dafc760054001fb84b0
yuanzhicheng <313640087@qq.com>
2020-11-14 02:43:23 +0800

    init

--- js/common.js
+++ js/common.js
@@ -80,7 +80,7 @@
         el: '#App',
         data: {
             tab: 'main',
-            contract_address: 'TLd3e6374Zt4FKAHAtuR2AsZpDMSAvY244',
+            contract_address: 'TRoXyBQk5T4GSgKnbpXnCqt8CsJbFAkQUx',
             defaultRef: 'TRX4zLRZBzD38WxcGBGmtXQtDh124bz8r4',
             upline: '',
             ref: '',
